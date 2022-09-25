ba67e67e4d3d2022b5e524ffb197c984aed3d1c0
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-09-25 16:57:19 +0900

    Create websocketServer.php

---
+++ websocketServer/src/websocketServer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use App\Chat;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+            )
+        ),
+    5000
+    );
+
+$server->run();
